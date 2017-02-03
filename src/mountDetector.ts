68ab8110581e72f6f2fafcac3653b79ead9b799b
Piotr Oleś <piotrek.oles@gmail.com>
2017-02-03 18:38:26 +0100

    More strict Action type

--- src/mountDetector.ts
+++ src/mountDetector.ts
@@ -1,6 +1,7 @@
+import { Action } from 'redux';
 import { Detector } from './Detector';
 
-export function mountDetector<S1, S2, A>(selector: (state: S1) => S2, detector: Detector<S2>): Detector<S1> {
+export function mountDetector<S1, S2, A extends Action>(selector: (state: S1) => S2, detector: Detector<S2>): Detector<S1> {
   return function mountedDetector(prevState: S1, nextState: S1): A[] | void {
     return detector<A>(selector(prevState), selector(nextState));
   };
