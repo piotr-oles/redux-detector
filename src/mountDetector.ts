14407515b769ff9a9a85ed7d455622f99fbc21d5
Piotr Oleś <piotr@realytics.io>
2017-05-19 08:18:55 +0200

    Version 0.6.0-1

--- src/mountDetector.ts
+++ src/mountDetector.ts
@@ -1,8 +1,7 @@
 import { Detector } from './Detector';
-import { ActionLike } from './ActionLike';
 
 export function mountDetector<S1, S2>(selector: (state: S1) => S2, detector: Detector<S2>): Detector<S1> {
-  return function mountedDetector(prevState: S1, nextState: S1): ActionLike | ActionLike[] | void {
+  return function mountedDetector(prevState: S1, nextState: S1): any | any[] | void {
     return detector(selector(prevState), selector(nextState));
   };
 }
--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,6 @@ var path = require('path');
 var webpack = require('webpack');
 var process = require('process');
 
-var env = process.env.NODE_ENV;
-
 var config = {
   context: __dirname,
   target: 'web',
@@ -55,26 +53,5 @@ var config = {
     }
   }
 };
-
-if ('production' === env) {
-  config.plugins.push(
-    new webpack.optimize.UglifyJsPlugin({
-      sourceMap: true,
-      compressor: {
-        pure_getters: true,
-        unsafe: true,
-        unsafe_comps: true,
-        warnings: false,
-        screw_ie8: false
-      },
-      mangle: {
-        screw_ie8: false
-      },
-      output: {
-        screw_ie8: false
-      }
-    })
-  );
-}
 
 module.exports = config;
