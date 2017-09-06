bbb8c02f03a0e92c6bc0c69abbc106829c3d9179
Piotr Oleś <piotr@realytics.io>
2017-09-06 12:25:47 +0200

    feat: 🎸 Upgrade libraries and add support for redux@^4.0.0

---
+++ src/lib/mountDetector.ts
@@ -0,0 +1,7 @@
+import { Detector } from '../Detector';
+
+export function mountDetector<TOuterState, TState>(selector: (state: TOuterState | undefined) => TState, detector: Detector<TState>): Detector<TOuterState> {
+  return function mountedDetector(prevState: TOuterState | undefined, nextState: TOuterState | undefined): any | any[] | void {
+    return detector(selector(prevState), selector(nextState));
+  };
+}
