b093b56263ecdc9e6b8def0c672f1e3d78c4a093
Piotr Oleś <piotr.oles@codibly.com>
2019-04-17 14:59:45 +0200

    feat: 🎸 Make action type less strict
    In order to be compatible with redux middlewares like redux-thunk, the
    default `TAction` type has been changed to any (it was `TAction extends
    AnyAction = AnyAction`).

--- src/Detector.ts
+++ src/Detector.ts
@@ -1,5 +1,3 @@
-import { Action, AnyAction } from "redux";
-
 /**
  * Function that reduces previous and next state to single value.
  */
@@ -11,10 +9,10 @@ export type Detector<TState = any, TResult = any> = (
 /**
  * Function that compares previous and next state and can return action, array of actions or nothing.
  */
-export type ActionsDetector<
-  TState = any,
-  TAction extends Action = AnyAction
-> = Detector<TState, TAction | TAction[] | void>;
+export type ActionsDetector<TState = any, TAction = any> = Detector<
+  TState,
+  TAction | TAction[] | void
+>;
 
 /**
  * Function that compares previous and next state and returns boolean value
